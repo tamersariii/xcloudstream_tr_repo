6d3865ed548e199385c6a0dba5854061667809e9
Tamer SARI <tamersariii@outlook.com>
2026-09-18 20:58:24 +0300

    Add files via upload

--- 720izle/build.gradle.kts
+++ 720izle/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "720izle.com üzerinden film izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie")
+    language = "tr"
+    iconUrl = "https://720izle.com/favicon.ico"
+}

423420c6d0880994b34651a4c5001009baeff000
Tamer SARI <tamersariii@outlook.com>
2026-09-18 20:59:14 +0300

    Add files via upload

--- FilmKutusu/build.gradle.kts
+++ FilmKutusu/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "FilmKutusu.com üzerinden film izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie")
+    language = "tr"
+    iconUrl = "https://filmkutusu.com/favicon.ico"
+}

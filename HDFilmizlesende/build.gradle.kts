9c0c5a6db514174713506f992adb5fc3aad06c34
Tamer SARI <tamersariii@outlook.com>
2026-09-18 21:03:25 +0300

    Add files via upload

--- HDFilmizlesende/build.gradle.kts
+++ HDFilmizlesende/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "HDFilmizlesende.com üzerinden film ve dizi izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "tr"
+    iconUrl = "https://hdfilmizlesende.com/favicon.ico"
+}

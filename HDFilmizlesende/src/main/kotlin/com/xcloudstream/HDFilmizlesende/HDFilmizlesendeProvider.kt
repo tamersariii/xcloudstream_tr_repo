0db025b54fed1d296c8009af6562b96db7d026e9
Tamer SARI <tamersariii@outlook.com>
2026-09-19 00:07:59 +0300

    Refactor extractor link creation in HDFilmizlesendeProvider

--- HDFilmizlesende/src/main/kotlin/com/xcloudstream/HDFilmizlesende/HDFilmizlesendeProvider.kt
+++ HDFilmizlesende/src/main/kotlin/com/xcloudstream/HDFilmizlesende/HDFilmizlesendeProvider.kt
@@ -2,6 +2,7 @@ package com.xcloudstream.HDFilmizlesende
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Element
 
@@ -83,14 +84,14 @@ class HDFilmizlesendeProvider : MainAPI() {
             if (src.isNotEmpty()) {
                 found = true
                 callback.invoke(
-                    newExtractorLink(
+                    ExtractorLink(
                         source = this.name,
                         name = "HDFilmizlesende",
-                        url = fixUrl(src)
-                    ) {
-                        this.quality = Qualities.Unknown.value
-                        this.referer = mainUrl
-                    }
+                        url = fixUrl(src),
+                        referer = mainUrl,
+                        quality = Qualities.Unknown.value,
+                        type = ExtractorLinkType.VIDEO
+                    )
                 )
             }
         }

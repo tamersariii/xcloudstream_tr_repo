f1de418d941238e743880d40eb5879b129072230
Tamer SARI <tamersariii@outlook.com>
2026-09-18 21:04:16 +0300

    Add files via upload

--- UltraFilmizle/build.gradle.kts
+++ UltraFilmizle/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "UltraFilmizle.org üzerinden film izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie")
+    language = "tr"
+    iconUrl = "https://ultrafilmizle.org/favicon.ico"
+}

c8330f0210befd09a4a41983339b96ddd90ac39f
Tamer SARI <tamersariii@outlook.com>
2026-09-18 21:01:44 +0300

    Add files via upload

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "HDFilmCehennemi.nl üzerinden film ve dizi izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "tr"
+    iconUrl = "https://www.hdfilmcehennemi.nl/favicon.ico"
+}

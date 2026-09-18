078aa567ebf39205070b27e4ae92f7e7b73fd3c9
Tamer SARI <tamersariii@outlook.com>
2026-09-18 21:00:03 +0300

    Add files via upload

--- FilmMakinesi/build.gradle.kts
+++ FilmMakinesi/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "FilmMakinesi.to üzerinden film ve dizi izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "tr"
+    iconUrl = "https://filmmakinesi.to/favicon.ico"
+}

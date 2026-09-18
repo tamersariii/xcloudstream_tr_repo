3b9f08cc0aae8a53d2c894e5356c4d6e95d8a840
Tamer SARI <tamersariii@outlook.com>
2026-09-18 21:00:56 +0300

    Add files via upload

--- FullHDFilmizlesene/build.gradle.kts
+++ FullHDFilmizlesene/build.gradle.kts
@@ -1,1 +1,8 @@
-
+cloudstream {
+    description = "FullHDFilmizlesene.now üzerinden film izleme"
+    authors = listOf("tamersariii")
+    status = 1
+    tvTypes = listOf("Movie")
+    language = "tr"
+    iconUrl = "https://www.fullhdfilmizlesene.now/favicon.ico"
+}
